e70199101973a02481b76ec54dcc204e3fc11f2e
ziovio <anderkoval3@gmail.com>
2020-04-09 12:59:01 +0300

    default layout done

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
-import Home from './views/Home';
-import List from './views/List';
+import { AsideNav } from './components/AsideNav';
+import { Test } from './views/Test';
+import { TopNav } from './components/TopNav';
+import { Footer } from './components/Footer';
 
 
-class App extends Component {
-  render() {
-    const App = () => (
-      <div>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/list' component={List}/>
-        </Switch>
+const App = () => {
+  const [navToggled, setNavToggled] = useState(true);
+  return (
+    <div id="wrapper">
+      <AsideNav navToggled={navToggled} />
+      <div id="content-wrapper" className="d-flex flex-column">
+        <div id="content">
+          <TopNav toggleAsideNav={() => setNavToggled(!navToggled)}/>
+          <div className="container-fluid">
+            <Switch>
+              <Route exact path='/home' component={Test} />
+              <Redirect to="/home" />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
       </div>
-    )
-    return (
-      <Switch>
-        <App/>
-      </Switch>
-    );
-  }
+    </div>
+  )
 }
+
 
 
 export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = (props) => {
+    return (
+        <footer className="sticky-footer bg-white">
+            <div className="container my-auto">
+                <div className="copyright text-center my-auto">
+                    <span>Copyright &copy; Your Website 2019</span>
+                </div>
+            </div>
+        </footer>
+    )
+}
