f341d0b68a082a0f97e7fdc86eb21524972b68ae
Андрей <anderkoval3@gmail.com>
2020-04-14 19:15:53 +0300

    init server

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+module.exports = {
+    DB_URL: process.env.MONGODB_URL,
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+    PORT: process.env.PORT || 5000,
+};
