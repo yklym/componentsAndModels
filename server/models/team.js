d5792d290a16923339e5f83a3e75d85ace3c21fd
Андрей <anderkoval3@gmail.com>
2020-04-20 17:30:57 +0300

    added bio into team nodel

--- server/models/team.js
+++ server/models/team.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const teamSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true }, 
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}], 
+    bio: { type: String, trim: true },
     documents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Document'}]
 });
 
