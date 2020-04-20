93a0e1f88c736a6c9c8ccd08ab814a026ffce5db
Андрей <anderkoval3@gmail.com>
2020-04-20 17:59:41 +0300

    added avaurl to user

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true, trim: true }, 
+    avaUrl: { type: String, trim: true },
     lastName: { type: String, required: true, trim: true }, 
     email: { type: String, required: true, trim: true },
     passwordHash: { type: String, required: true }
